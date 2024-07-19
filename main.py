de2554acefdb41e8b01cc841076134395f8bb29c
orlandok1 <orlando_koolaard@hotmail.com>
2024-07-19 14:27:53 +0200

    Added get char dict function

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    chars_dict = get_char_dict(text)
     print(f"{num_words} words found in the document")
+    print(chars_dict)
  
 def get_book_text(path):
     with open(path) as f:
@@ -12,5 +14,16 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
+def get_char_dict(text):
+    chars = {}
+    for char in text:
+        lowered = char.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] =1
+    return chars
 
+main()
+    
 
